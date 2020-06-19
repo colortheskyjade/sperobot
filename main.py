8854e40292e5c797351d8187dd5b9991f9bad703
Ruyan Chen <colortheskyjade@noreply.users.github.com>
2020-06-19 15:23:26 -0700

    Hello world

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+
+from config import API_KEY
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(API_KEY)
