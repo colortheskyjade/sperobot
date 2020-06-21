e45ad4141d5cbac71cbaf6b3cea28a07d9ebef76
Ruyan Chen <colortheskyjade@users.noreply.github.com>
2020-06-20 21:32:49 -0700

    Add logging & game activity

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import discord
+import google.cloud.logging
+import logging
+import os
 
 from config import API_KEY
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+isDev = os.getenv('SPERO_BOT_DEV', False)
+print(isDev)
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+logging.basicConfig(level=logging.INFO)
+if not isDev:
+  client = google.cloud.logging.Client()
+  client.setup_logging(logging.INFO)
 
-client = MyClient()
+logger = logging.getLogger(__name__)
+
+class BotClient(discord.Client):
+  
+  async def on_ready(self):
+    logger.info('Logged on as {0}!'.format(self.user))
+    await self.change_presence(activity=discord.Game('with your heart, YO!'))
+
+  async def on_message(self, message):
+    # logger.info('Message from {0.author}: {0.content}'.format(message))
+    pass
+
+client = BotClient()
 client.run(API_KEY)
